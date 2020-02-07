2611f44138ca92a134ad7970622ee03404fd5137
Warwick Stone <uozu.aho@gmail.com>
2020-02-07 22:26:47 +1100

    extract logging decorator

--- src/async/AsyncLifecycle.tsx
+++ src/async/AsyncLifecycle.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { logPureLifecycle } from '../utils/log_decorators';
 
 @logPureLifecycle
-export class LifecycleDemos extends React.PureComponent {
+export class AsyncLifecycleDemo extends React.PureComponent {
     public render() {
         return <h1>LifecycleDemos</h1>;
     }
---
+++ src/utils/log_decorators.ts
@@ -0,0 +1,23 @@
+export const logPureLifecycle = (constructor: Function) => {
+    const original = constructor.prototype.render;
+
+    constructor.prototype.render = function() {
+        console.log(`${constructor.name}: render`);
+        return original.apply(this, arguments);
+    };
+
+    constructor.prototype.componentDidMount = function() {
+        console.log(`${constructor.name}: componentDidMount`);
+        return original.apply(this, arguments);
+    };
+
+    constructor.prototype.componentWillUnmount = function() {
+        console.log(`${constructor.name}: componentWillUnmount`);
+        return original.apply(this, arguments);
+    };
+
+    constructor.prototype.componentDidUpdate = function() {
+        console.log(`${constructor.name}: componentDidUpdate`);
+        return original.apply(this, arguments);
+    };
+}
