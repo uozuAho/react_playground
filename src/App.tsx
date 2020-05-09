96746821144d8bc86c9b67d361b62fc3b95f346a
Warwick Stone <uozu.aho@gmail.com>
2020-05-09 19:00:13 +1000

    add sidebar router

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 import { LifecycleDemos } from './lifecycle/LifecycleDemos';
 import { SettingsDoover } from './prop_drilling/bad_example/Settings';
+import {
+  SidebarRouter,
+  SidebarItem,
+  ISidebarItem
+} from './layout/SidebarRouter';
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        {/* <LifecycleDemos /> */}
-        <SettingsDoover />
-      </header>
+      <SidebarRouter items={componentRoutes}/>
     </div>
   );
 }
+
+const componentRoutes: ISidebarItem[] = [
+  new SidebarItem(
+      "/lifecycle_demos",
+      "Lifecycle demos",
+      () => <LifecycleDemos />
+  ),
+  new SidebarItem(
+    "/prop_drilling",
+    "Prop drilling",
+    () => <SettingsDoover />
+  ),
+];
 
 export default App;
