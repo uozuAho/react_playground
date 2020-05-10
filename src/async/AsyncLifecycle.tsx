04044b7f5706be0aae14fe4b98aac51c18cfc9d3
Warwick Stone <Warwick.Stone@myob.com>
2020-05-10 20:33:47 +1000

    docs

--- src/async/AsyncLifecycle.tsx
+++ src/async/AsyncLifecycle.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import { logPureLifecycle } from '../utils/log_decorators';
 
+/**
+ * I can't remember what I was trying to do here. Probably trying
+ * to figure out what happened when you made react's lifecycle
+ * methods async
+ */
 @logPureLifecycle
 export class AsyncLifecycleDemo extends React.PureComponent {
     public render() {
