b498fc5c03065b557519df5a0b8d46c5ffa6173a
Warwick Stone <uozu.aho@gmail.com>
2020-05-23 22:18:08 +1000

    WIP: trying to make react context work

--- src/prop_drilling/PropDrillingDemo.tsx
+++ src/prop_drilling/PropDrillingDemo.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { BuggySettings } from "./buggy_example/BuggySettings";
 import { MarkdownDisplay } from "../utils/MarkdownDisplay";
 import { PropDrillingSettings } from "./prop_drilling_fix/PropDrillingSettings";
+import { ContextSettings } from "./context_fix/ContextSettings";
 
 const readme = require('./readme.md');
 
@@ -10,8 +11,12 @@ export const PropDrillingDemo = () => {
   return (
     <>
       <MarkdownDisplay mdImport={readme} />
+      <hr />
       <BuggySettings />
+      <hr />
       <PropDrillingSettings />
+      <hr />
+      <ContextSettings />
     </>
   );
 };
---
+++ src/prop_drilling/context_fix/SettingsViewer.tsx
@@ -0,0 +1,17 @@
+import * as React from "react"
+
+import { Settings } from "./ContextSettings"
+
+interface SettingsViewerProps {
+  settings: Settings
+};
+
+export const SettingsViewer: React.FunctionComponent<SettingsViewerProps> = props => {
+  
+  return (
+    <ul>
+      <li>Enabled: {props.settings.enabled ? 'Yep': 'Nup'}</li>
+      <li>Selected template: {props.settings.selectedTemplateId}</li>
+    </ul>
+  );
+}
