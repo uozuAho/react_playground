db9da79289e618ca34a9f10e9d9cf504c46fe9aa
Warwick Stone <uozu.aho@gmail.com>
2020-06-21 11:43:29 +1000

    add testing library test

---
+++ src/App.spec.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { render, fireEvent, waitFor, screen } from '@testing-library/react'
+import App from './App';
+
+test('loads and displays greeting', async () => {
+  render(<App />);
+
+  fireEvent.click(screen.getByText('Lifecycle demos'));
+
+  await waitFor(() => screen.getByRole('heading'));
+
+  expect(screen.getByRole('heading')).toHaveTextContent('LifecycleDemos');
+});
