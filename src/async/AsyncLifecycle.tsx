95736ff5b5261d96cb91fdc039bb705cbd2a3346
Warwick Stone <warwick_stone@fastmail.com>
2022-09-11 11:47:01 +1000

    lint

--- src/async/AsyncLifecycle.tsx
+++ src/async/AsyncLifecycle.tsx
@@ -8,7 +8,7 @@ import { logPureLifecycle } from '../utils/log_decorators';
  */
 @logPureLifecycle
 export class AsyncLifecycleDemo extends React.PureComponent {
-    public render() {
-        return <h1>LifecycleDemos</h1>;
-    }
+  public render() {
+    return <h1>LifecycleDemos</h1>;
+  }
 }
