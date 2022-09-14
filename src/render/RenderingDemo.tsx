6a5265d7c1c9a7460fc1cb9f1bbffd0f2179d8f9
Warwick Stone <warwick_stone@fastmail.com>
2022-09-14 15:14:34 +1000

    WIP

--- src/render/RenderingDemo.tsx
+++ src/render/RenderingDemo.tsx
@@ -1,37 +1,70 @@
+/**
+ * This page is easier to read in a browser. Run `npm start` and go to
+ * http://localhost:3000/render
+ */
+
 import React from "react";
 
-export function RenderingDemo() {
-  return (
-    <>
-      <h1>Rendering</h1>
+export class RenderingDemo extends React.PureComponent {
+  render(): React.ReactNode {
+    console.log('render RenderingDemo');
 
-      <p>Use chrome + react dev tools. Go to dev tools - components - settings
-        - highlight updates when components render. If you also want to see DOM
-        paints, click the chrome dev tools ellipses - more tools - rendering -
-        paint flashing.
-      </p>
+    return (
+      <>
+        <h1>Rendering</h1>
 
-      <h2>To do</h2>
-      <ul>
-        <li>MemoCounterExample: counter B still shows a component render, but doesn't log it. Why?</li>
-      </ul>
+        <p>Use chrome + react dev tools. Go to dev tools - components - settings
+          - highlight updates when components render<sup>1</sup>. If you also want to see DOM
+          paints, click the chrome dev tools ellipses - more tools - rendering -
+          paint flashing.
+        </p>
 
-      <FirstBadExample />
-      <MemoCounterExample />
-      <ClassCounterExample />
-    </>
-  );
+        <h2>To do</h2>
+        <ul>
+          <li>object as prop example and mitigation</li>
+          <li>Why do all button clicks cause a full page paint?</li>
+        </ul>
+
+        <h2>Quirks</h2>
+        <ol>
+          <li><a href="https://github.com/facebook/react/issues/19778">React dev tools bug/quirk</a>:
+          'Hightlight updates' appears to highlight whenever render _would_ be
+          called, but it may not be. This can be confirmed with the profiler /
+          render counter.</li>
+        </ol>
+
+        <hr></hr>
+
+        <FirstBadExample />
+        <MemoCounterExample />
+
+        <hr></hr>
+        <h1>References</h1>
+        <ul>
+          <li>
+            <a href="https://felixgerschau.com/react-rerender-components/">
+              https://felixgerschau.com/react-rerender-components/</a>
+          </li>
+        </ul>
+      </>
+    );
+  }
 }
 
 function FirstBadExample() {
   const [counterA, setCounterA] = React.useState(0);
   const [counterB, setCounterB] = React.useState(0);
 
+  console.log("render FirstBadExample");
+
   return (
     <>
-      <h2>Example one - unnecessary rendering!</h2>
+      <h2>Unnecessary rendering of child components</h2>
       <p>When you increment the counter, all components under this example
-        are updated, and the whole page paints!.
+        are rendered. This is because changes to a component's state cause all
+        child components to update, regardless of whether their state or props
+        changed. Note that React's default component behaviour is to render on
+        any props or state change (<a href="https://reactjs.org/docs/react-component.html#shouldcomponentupdate">React docs</a>).
       </p>
 
       <button
@@ -43,6 +76,7 @@ function FirstBadExample() {
 
       <Counter name="A" counter={counterA}/>
       <Counter name="B" counter={counterB}/>
+      <RenderCounter />
     </>
   );
 }
@@ -51,12 +85,14 @@ function MemoCounterExample() {
   const [counterA, setCounterA] = React.useState(0);
   const [counterB, setCounterB] = React.useState(0);
 
+  console.log("render MemoCounterExample");
+
   return (
     <>
-      <h2>Example two - React.memo</h2>
+      <h2>MemoCounterExample</h2>
 
       <p>This example uses React.memo around the counter component to ensure it
-        is only rendered when the value changes.
+        is only rendered when its prop value changes.
       </p>
 
       <button
@@ -68,52 +104,24 @@ function MemoCounterExample() {
 
       <MemoCounter name="A" counter={counterA}/>
       <MemoCounter name="B" counter={counterB}/>
+      <RenderCounter />
     </>
   );
 }
 
-interface ClassCounterExampleState {
-  counterA: number,
-  counterB: number
-}
+function RenderCounter() {
+  const counter = React.useRef(0);
 
-class ClassCounterExample extends React.Component<{}, ClassCounterExampleState> {
-  constructor(props: any) {
-    super(props);
-    this.state = {
-      counterA: 0,
-      counterB: 0
-    };
-  }
-
-  private onClickA = () => {
-    this.setState(state => ({
-        counterA: state.counterA + 1
-      })
-    );
-  }
-
-  render(): React.ReactNode {
-    return (
-      <>
-        <h2>ClassCounterExample</h2>
-  
-        <p>todo: explain me
-        </p>
-  
-        <button onClick={this.onClickA}>Increment counter A</button>
-        <p>A: {this.state.counterA}</p>
-        <p>B: {this.state.counterB}</p>
-      </>
-    );
-  }
+  return (
+    <b>Renders: {++counter.current}</b>
+  )
 }
 
 function Counter({name, counter}: {name: string, counter: number}) {
   console.log(`Rendering counter ${name}`);
   return (
     <>
-      <p>{name}: {counter}</p>
+      <p>{name}: {counter} <RenderCounter /></p>
     </>
   )
 }
@@ -121,8 +129,8 @@ function Counter({name, counter}: {name: string, counter: number}) {
 const MemoCounter = React.memo(function Counter({ name, counter }: {name: string, counter: number}) {
   console.log(`Rendering counter ${name}`);
   return (
-    <div>
-      {name}: {counter}
-    </div>
+    <>
+      <p>{name}: {counter} <RenderCounter /></p>
+    </>
   );
 });
